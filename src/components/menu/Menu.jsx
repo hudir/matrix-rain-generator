1c97e3489828905088f7d52dd4cbd22f7c5d858d
hudir <hudirybw@gmail.com>
2022-06-22 12:03:51 +0200

    fix some bugs

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import Store from '../../store/store'
 import "./menu.scss"
 
 
 export default function Menu() {
+  const {currentPage} = useContext(Store)
   
   return (
     <div className='menu'>
         
         <nav>
-         <Link to="setting">Setting</Link>
-         <Link to="code">Show Code</Link>
-         <Link to="about">About</Link>
-         <Link to="contact">Contact</Link>
+         <Link className={currentPage==="setting" ? "activ" : ""} to="setting">Setting</Link>
+         <Link className={currentPage==="code" ? "activ" : ""} to="code">Show Code</Link>
+         <Link className={currentPage==="about" ? "activ" : ""} to="about">About</Link>
+         <Link className={currentPage==="contact" ? "activ" : ""} to="contact">Contact</Link>
+         <br />
+         <small>&copy; 2022, Zhuo Yang</small>
         </nav>
+        
     </div>
   )
 }
--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./sidebar.scss"
+import { useNavigate } from "react-router-dom";
 
 export default function Sidebar() {
+  const go = useNavigate()
   return (
     <div className="side">
-         <div className='heading'>
+         <div className='heading' onClick={e=>go('/about')}>
             <h1>Matrix Rain Generator</h1>
             <h2>for create-react-app</h2>
         </div>
